8adba3148dfd8540aaf123975d3a7888814d97da
Sujay Muramalla <sujay.muramalla@gmail.com>
2026-02-07 11:43:09 +0100

    updated new assignments in DS_session1.py

---
+++ DS_Session_1.py
@@ -0,0 +1,17 @@
+#functions - circular bracket 
+#indexes - square brackets
+#objects - curly brackets
+
+print ("hello")
+
+#colab - run the code using shift + enter
+#python is a case sensitive language
+
+#variable: right to left 
+age=28
+#variables are case sensitive 
+#among all symboles, only _ is allowed in variable names
+age_man = 29 
+_age=34
+print (_age)
+
